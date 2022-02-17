978107f638a8b09066ef8531ad88dad51414bc29
SOFTSERVE\mkork <mkork@softserveinc.com>
2022-02-17 10:23:23 +0200

    added android configuration

---
+++ src/test/java/Asd.java
@@ -0,0 +1,9 @@
+import com.sp.pet.mobile.automation.util.runners.AndroidTestRunner;
+import org.junit.jupiter.api.Test;
+
+public class Asd extends AndroidTestRunner {
+    @Test
+    public void sadf(){
+        System.out.println("asdd");
+    }
+}
